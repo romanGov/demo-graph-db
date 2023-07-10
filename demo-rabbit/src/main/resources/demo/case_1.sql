83eda33e29135512751b16d2d7874a38f30a14a2
roman.gaponov <roman.gaponov@payallps.com>
2023-07-10 18:24:28 +0300

    fixed

--- demo-rabbit/src/main/resources/demo/case_1.sql
+++ demo-rabbit/src/main/resources/demo/case_1.sql
@@ -1,7 +1,7 @@
 --who is working on more than one position
 select business_name, person_id, count(wi.business_id) as business_count
-from graph.business b
-         join graph.works_in wi on b.business_id = wi.business_id
+from demo_graph.business b
+         join demo_graph.works_in wi on b.business_id = wi.business_id
 group by b.business_id, person_id
 having count(wi.business_id)>1
 order by business_count desc;
--- demo-rabbit/src/main/resources/demo/case_2.sql
+++ demo-rabbit/src/main/resources/demo/case_2.sql
@@ -1,7 +1,7 @@
 --who pays for more than one position
 select c.first_name, c.customer_id, count(pf.customer_id) as customer_count
-from graph.customer c
-         join graph.pays_for pf on c.customer_id = pf.customer_id
+from demo_graph.customer c
+         join demo_graph.pays_for pf on c.customer_id = pf.customer_id
 group by c.customer_id, pf.service_id
 having count(pf.customer_id)>1
 order by c.first_name;
